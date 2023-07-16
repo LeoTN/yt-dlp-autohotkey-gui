20141c383dca71ee38c2a418f44c5f662878461b
Donnerbaer <37273909+LeoTN@users.noreply.github.com>
2023-07-16 15:37:42 +0200

    reworked powershell output
    The powershell output window will now be more CPU efficient as it will only check every 100ms for an updated download log.
    It also skips empty lines to create a smooth looking log flow.

--- files/library/MonitorHookFile.ps1
+++ files/library/MonitorHookFile.ps1
@@ -2,9 +2,27 @@ $Host.UI.RawUI.BackgroundColor = "Black"
 $Host.UI.RawUI.ForegroundColor = "Green"
 Clear-Host
 $logfile = $env:temp + "\yt_dlp_download_log.txt"
+Write-Host "Terminal ready..."
 
-# Creates an infinite loop to display the text file content.
-while ($true) {
-    Clear-Host
-    Get-Content -Path $logfile -Wait
+$currentLineCount = (Get-Content -Path $logfile).Count
+
+# Creates an infinite loop to display new text file content.
+While ($true) {
+    $content = Get-Content -Path $logfile
+    $newLineCount = $content.Count
+
+    # Checks if new lines have arrived.
+    If ($newLineCount -gt $currentLineCount) {
+        $newLines = $content[$currentLineCount..($newLineCount - 1)]
+        Foreach ($line in $newLines) {
+            If ($line -ne "") {
+                Write-Host $line
+            }
+        }
+        $currentLineCount = $newLineCount
+    }
+    Else {
+        # Waits for new lines to arrive.
+        Start-Sleep -Seconds 0.1
+    }
 }
